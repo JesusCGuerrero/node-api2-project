3b6540504b8eaa78772da3903bb128ffe873e492
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-05-06 05:32:51 -0400

    fixes

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -4,13 +4,6 @@ const db = require("../data/db");
 
 const router = express.Router()
 
-let Post = {
-    title: "", // String, required
-    contents: "", // String, required
-    created_at: Date.now(), // Date, defaults to current date
-    updated_at: Date.now() // Date, defaults to current date
-}
-
 let comment = {
     text: "", // String, required
     post_id: "", // Integer, required, must match the id of a post entry in the database
@@ -32,7 +25,7 @@ router.get("/:id", (req, res) => {
     const id = req.params.id
     db.findById(id)
     .then(post => {
-        if (!post.id) {
+        if (post.length == 0) {
             res.status(404).json({error: "The post with the specified ID does not exist."})
         } else {
             res.status(200).json(post);
@@ -59,49 +52,55 @@ router.get("/:id/comments", (req, res) => {
 })
 
 router.post("/", (req, res) => {
-    Post.title = req.body.title,
-    Post.contents = req.body.contents
-    db.insert(Post)
-    .then(post => {
-        if(req.body.contents.length==0 || req.body.title.length==0){
-            res.status(404).json({errorMessage: "Please provide title and contents for the post."})
-        } else {
-            res.status(200).json({post: post})
-        }
-    }).catch(error => {
-        res.status(500).json({error: "The posts information could not be retrieved."}).end()
-    })
+    if (!req.body.title || !req.body.contents) {
+        res.status(400).json({message: 'please make sure to send a valid title and valid contents'})
+      } else {
+        db.insert(req.body)
+            .then(post => {
+                    res.status(201).json(post)
+            }).catch(error=>{
+                console.log(error)
+                res.status(500).json({errormessage: 'error getting data'})
+            })
+      }
 })
 
 router.post("/:id/comments", (req, res) => {
-    comment.text = req.body.text,
-    comment.post_id = req.body.post_id
-    const id = req.params.id
-    db.insertComment(comment)
-    .then(comment => {
-        if(req.body.post_id == (id)){
-            res.status(201).json(comment)
-        } else if(req.body.text = 0) {
+        const id = req.params.id
+        comment = req.body
+        comment.post_id = Number(id)
+
+        if (!req.body.text) {
             res.status(400).json({errorMessage: "Please provide text for the comment."})
         } else {
-            res.status(404).json({message: "The post with the specified ID does not exist."})
+            db.insertComment(comment)
+            .then(comment => {
+                res.status(201).json(comment)
+            }).catch(error=>{
+                console.log(error)
+                res.status(404).json({message: "The post with the specified ID does not exist."})
+            })
         }
-    }).catch(error => {
-        res.status(500).json({error: "The posts information could not be retrieved."}).end()
-    })
 })
 
 router.put("/:id", (req, res) => {
     const id = req.params.id
-    let postToUpdate = {
-        title: req.body.title,
-        contents: req.body.contents,
-        id: id
+    let post = req.body
+
+    if(!req.body.title || !req.body.contents){
+        res.status(400).json({message: 'provide a title and contents for the post.' })
+    } else {
+        db.update(id, post)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: 'The post with the specified ID does not exist, please try again.' })
+            }
+        }).catch(error => {
+            res.status(500).json({ error: 'The post information could not be modified' })
+        })
     }
-    db.update(id, postToUpdate)
-    .then(post => {
-        res.status(200).json(postToUpdate)
-    })
 })
 
 router.delete("/:id", (req, res) => {
